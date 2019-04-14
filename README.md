68d168db5b21b5deb240b2666a75d972ab9378f6
Daniel Manfred <danielmanfred@gmail.com>
2019-04-14 00:43:13 -0300

    Create router sigle series

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# TV Series App [Application under construction]
+# TV Series App [ Here you can find all of your most loved series ]
+
+## How to run this application
+
+1. paste the project: git clone https://github.com/danielmanfred/tv-series-app.git
+2. enter app: cd tv-series-app
+3. install the dependencies: yarn
+4. run the API: yarn start
+
+## Resources used in development
+
+- React
+- React Router
+- TVMaze API
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Intro from './../Intro';
-import Series from './../../containers/Series';
+import Main from './../Main'
 import './App.css';
 
 class App extends Component {
@@ -10,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Tv Series List</h1>
         </header>
-        <Intro message="Here you can find all of your most loved series" />
-        <Series />
+        <Main />
       </div>
     );
   }
